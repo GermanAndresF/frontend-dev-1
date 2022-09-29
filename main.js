fdcdc5dd0ac42fcd8d858725a105368e559c6e78
German Flechas <gfgermanflechas5@gmail.com>
2022-09-29 14:43:18 -0500

    Carrito de compras

--- main.js
+++ main.js
@@ -1,15 +1,31 @@
-const menuEmail = document.querySelector('.navbar-email');
-const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu');
-const mobileMenu = document.querySelector('.mobile-menu');
+// Variables desktop
+const emailMenu = document.querySelector(".navbar-email");
+const desktopMenu = document.querySelector(".desktop-menu");
 
-menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMobileMenu);
+// Variables mobile
+const burguerMenu = document.querySelector(".menu");
+const mobileMenu = document.querySelector(".mobile-menu");
 
-function toggleDesktopMenu(){
-    desktopMenu.classList.toggle('inactive');
+// Variables mixtas
+const shoppingCart = document.querySelector(".navbar-shopping-cart");
+const productDetail = document.querySelector(".product-detail");
+
+emailMenu.addEventListener("click", showDesktopMenu);
+burguerMenu.addEventListener("click", showMobileMenu);
+shoppingCart.addEventListener("click", showMyOrder);
+
+function showDesktopMenu() {
+  desktopMenu.classList.toggle("inactive");
+  productDetail.classList.add("inactive");
 }
 
-function toggleMobileMenu(){
-    mobileMenu.classList.toggle('inactive');
+function showMobileMenu() {
+  mobileMenu.classList.toggle("inactive");
+  productDetail.classList.add("inactive");
+}
+
+function showMyOrder() {
+  productDetail.classList.toggle("inactive");
+  mobileMenu.classList.add("inactive");
+  desktopMenu.classList.add("inactive");
 }
